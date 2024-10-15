7380fe777f6ab3dc94147631c0968428c59d13a0
hikari <95992625+hoagdead@users.noreply.github.com>
2024-10-15 15:48:43 +0700

    Add files via upload

---
+++ base/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+#path('tên trang/', view.tên của trang trong views, name = "tên ngắn gọn của trang")
+urlpatterns = [
+    #url cho trang login/logout/register 
+    path('logout/', views.Logoutuser, name="logout"),
+    path('login/', views.Loginpage, name="login"),
+    path('register/', views.registerpage, name="register"),
+    
+    #url cho page chính và từng room
+    path('', views.home , name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
+    
+    #url khi muốn cập nhật room
+    path('create-room/', views.createroom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateroom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+    
+    #url cho profile
+    path('profile/', views.userprofile, name="profile"),
+    
+]
